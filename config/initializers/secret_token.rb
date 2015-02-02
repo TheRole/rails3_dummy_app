706335fd5dddbcb3d644ef4e65eb8adfdda60600
Ilya N. Zykin <zykin-ilya@ya.ru>
2015-02-03 00:50:39 +0300

    blank_rails_3_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheRails3::Application.config.secret_token = 'c51e74b0266efe25f58195de8f670b8e19b365d0208f8498fbe27d8de5dd5e95ab6ec50bb32d94d169037f09c0d1b9025f4f72df6e491b88bf06b34dd0f54b47'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TheRails3::Application.config.session_store :cookie_store, key: '_the_rails_3_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TheRails3::Application.config.session_store :active_record_store
